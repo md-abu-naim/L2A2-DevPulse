be82fca8d8ef293166f950f94d647a7ebce85e34
abunaim34 <farhanadnanfarabi@gmail.com>
2026-05-20 21:01:28 +0600

    NeonDB connect in my devpulse

--- src/db/index.ts
+++ src/db/index.ts
@@ -5,6 +5,24 @@ export const pool = new Pool({
     connectionString: config.db_connect
 })
 
-export const initDB = async() => {
+export const initDB = async () => {
+    try {
+        await pool.query(`
+        CREATE TABLE IF NOT EXISTS users(
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(25),
+        email VARCHAR(30) UNIQUE NOT NULL,
+        password TEXT NOT NULL,
+        role VARCHAR(20),
 
+        created_at TIMESTAMP DEFAULT NOW(),
+        updated_at TIMESTAMP DEFAULT NOW()
+        )
+        `)
+
+
+        console.log('Database connected successfully');
+    } catch (error) {
+        console.log(error);
+    }
 }
